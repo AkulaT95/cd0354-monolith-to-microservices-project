9b46c0352de894c4bd3c0f1c6b29e7eb2ca01f8f
Tuan Dang Ho Anh <dangtuansapper@gmail.com>
2024-06-02 12:02:10 +0700

    update frontend environment

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9db8f55a7f404540a03a9ed21ecf683-499506061.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
